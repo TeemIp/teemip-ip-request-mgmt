dfb294ea13820245527e7957c115c0466d3f3cb2
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2020-04-30 21:03:27 +0200

    Last updates for rev 2.6.1

--- teemip-portal/module.teemip-portal.php
+++ teemip-portal/module.teemip-portal.php
@@ -23,7 +23,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'teemip-portal/2.6.0',
+	'teemip-portal/2.6.1',
 	array(
 		// Identification
 		'label' => 'Enhanced Portal for TeemIp',
