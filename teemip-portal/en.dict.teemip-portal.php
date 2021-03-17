603ccb66f6517330d7bfe857441ad304f4665c3b
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2021-03-17 14:33:41 +0100

    Enhancements for 2.7.1

--- teemip-portal/en.dict.teemip-portal.php
+++ teemip-portal/en.dict.teemip-portal.php
@@ -1,23 +1,6 @@
 <?php
-// Copyright (C) 2020 TeemIp
-//
-//   This file is part of TeemIp.
-//
-//   TeemIp is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU Affero General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   TeemIp is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with TeemIp. If not, see <http://www.gnu.org/licenses/>
-
-/**
- * @copyright   Copyright (C) 2020 TeemIp
+/*
+ * @copyright   Copyright (C) 2021 TeemIp
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -25,4 +8,5 @@ Dict::Add('EN US', 'English', 'English', array(
 	'portal:teemip-portal' => 'IP portal',
 	'Brick:Portal:QuickNewTicket:Title' => 'Create an IP ticket',
 	'Brick:Portal:QuickNewTicket:Title+' => '<p>Need help?</p><p>Select a type of request, fill it and submit it to our support teams.</p>',
+	'Brick:Portal:OngoingRequests:Tab:Recent' => 'Recent',
 ));
--- teemip-portal/fr.dict.teemip-portal.php
+++ teemip-portal/fr.dict.teemip-portal.php
@@ -1,23 +1,6 @@
 <?php
-// Copyright (C) 2020 TeemIp
-//
-//   This file is part of TeemIp.
-//
-//   TeemIp is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU Affero General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   TeemIp is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with TeemIp. If not, see <http://www.gnu.org/licenses/>
-
-/**
- * @copyright   Copyright (C) 2020 TeemIp
+/*
+ * @copyright   Copyright (C) 2021 TeemIp
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
@@ -25,4 +8,5 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'portal:teemip-portal' => 'Portail IP',
 	'Brick:Portal:QuickNewTicket:Title' => 'Créer un ticket IP',
 	'Brick:Portal:QuickNewTicket:Title+' => '<p>Besoin d\'aide ?</p><p>Selectionnez un type de ticket, renseignez le et soumettez le à notre équipe de support.</p>',
+	'Brick:Portal:OngoingRequests:Tab:Recent' => 'Récentes',
 ));
