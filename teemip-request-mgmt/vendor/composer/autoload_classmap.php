f65edecada40432b24cbe5febeb2fea770a5d58a
xtophe38 <christophe.naud@gmail.com>
2023-05-17 11:21:54 +0200

    Use internal stimulus for ev_resolve now -> iApplicationUIExtension is not needed anymore

--- teemip-request-mgmt/vendor/composer/autoload_classmap.php
+++ teemip-request-mgmt/vendor/composer/autoload_classmap.php
@@ -7,7 +7,6 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
-    'TeemIp\\TeemIp\\Extension\\IPRequestManagement\\Hook\\IPRequestPlugIn' => $baseDir . '/src/Hook/IPRequestPlugIn.php',
     'TeemIp\\TeemIp\\Extension\\IPRequestManagement\\Model\\_IPRequest' => $baseDir . '/src/Model/_IPRequest.php',
     'TeemIp\\TeemIp\\Extension\\IPRequestManagement\\Model\\_IPRequestAddressCreateV4' => $baseDir . '/src/Model/_IPRequestAddressCreateV4.php',
     'TeemIp\\TeemIp\\Extension\\IPRequestManagement\\Model\\_IPRequestAddressCreateV6' => $baseDir . '/src/Model/_IPRequestAddressCreateV6.php',
