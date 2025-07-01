f9f0e0aff2f5bf4924846b88af148d64812e3a13
adrinaopsilva <adriano.psilva@outlook.com>
2025-07-01 16:21:39 -0400

    Added translation files and translations for Brazilian Portuguese (pt_br)

---
+++ teemip-portal/dictionaries/pt_br.dict.teemip-portal.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2023 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('PT BR', 'Brazilian', 'Brazilian', array(
+	'portal:teemip-portal' => 'Portal de IP',
+	'Brick:Portal:QuickNewTicket:Title' => 'Criar um ticket de IP',
+	'Brick:Portal:QuickNewTicket:Title+' => '<p>Precisa de ajuda?</p><p>Selecione um tipo de requisição, preencha-a e envie para nossas equipes de suporte.</p>',
+	'Brick:Portal:OngoingRequests:Title' => 'Requisições em andamento',
+	'Brick:Portal:OngoingRequests:Title+' => '<p>Acompanhe suas requisições em andamento.</p><p>Verifique o progresso, adicione comentários, anexe documentos e aprove a solução.</p>',
+	'Brick:Portal:OngoingRequests:Tab:Recent' => 'Recentes',
+	'Brick:Portal:OngoingRequests:Tab:OnGoing' => 'Abertas',
+	'Brick:Portal:OngoingRequests:Tab:Resolved' => 'Resolvidas',
+	'Brick:Portal:ClosedRequests:Title' => 'Requisições fechadas',
+	'Brick:Portal:ClosedRequests:Title+' => '',
+	'Brick:Portal:ClosedRequests:Tab:Recent' => 'Recentes',
+	'Brick:Portal:ClosedRequests:Tab:Closed' => 'Fechadas',
+));
